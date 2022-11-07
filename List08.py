55d86b8733df0c345ff8a05d6e1e97c03a4ccb0f
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-11-07 15:37:43 +0500

    08

--- List08.py
+++ List08.py
@@ -6,4 +6,15 @@ def main(fruits):
     Returns:
         list: return answer
     """
-    return
+    i = 0
+    n = 0
+ 
+    while i<len(fruits):
+        if fruits[i]=="apple":
+
+         n = fruits.pop(i)
+         i+=1
+    return fruits
+
+   
+print(main(["apple", "banana", "apple", "pear", "apple"]))   
