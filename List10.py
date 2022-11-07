e7160a4b1ba26a2400fc84e3a9867d90e068ef49
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-11-07 16:06:54 +0500

    10

--- List10.py
+++ List10.py
@@ -6,4 +6,14 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    i = 0
+    a = 0
+    b = 0
+    while i<len(list1):
+        if list1[i]==0:
+            a+=1
+        else :
+            b+=1
+        i+=1
+    return b,a
+print(main([1, 0, 0, 0, 1, 0, 1, 0]))
